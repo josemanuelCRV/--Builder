576695e4cfff88d5050e18eedfafe0fc66c33a80
Jose Manuel <josemanuel.crv@gmail.com>
2017-09-17 18:19:52 +0200

    style():formatted code and enhanced log message

--- app.js
+++ app.js
@@ -8,217 +8,225 @@ var rimraf = require('rimraf');
 var app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 
-app.post('/passdata', function (req, res) {
-	console.log(`printing ${JSON.stringify(req.body, null, 2)}`);
-	
-	const archselector = req.body.archetypeSelector;
-	console.log(`[POST]: Recived archetype: ` + archselector);
+app.post('/passdata', function(req, res) {
+    console.log(`printing ${JSON.stringify(req.body, null, 2)}`);
 
-	// Commons fields
-	const groupId = req.body.groupId;
-	const artifactId = req.body.artifactId;
-	const version = req.body.version;
-	const packageName = req.body.packageName;
+    const archselector = req.body.archetypeSelector;
+    console.log(`[POST]: Recived archetype: ` + archselector);
 
-	// Lite fields
-	const flowNameFolder = req.body.flowNameFolder;
-	const fuid = req.body.fuid;
-	const sbpVersion = req.body.sbpVersion;
-	const action1 = req.body.action1;
-	const action1Composite = req.body.action1Composite;
-	const servicio1Name = req.body.servicio1Name;
-	const servicio1Version = req.body.servicio1Version;
+    // Commons fields
+    const groupId = req.body.groupId;
+    const artifactId = req.body.artifactId;
+    const version = req.body.version;
+    const packageName = req.body.packageName;
 
-	// SAP fields
-	const type = req.body.type;
-	const servicioName = req.body.servicioName;
-	const servicioVersion = req.body.servicioVersion;
-	const cacheable = req.body.cacheable;
-	const din = req.body.in;
-	const sapVersion = req.body.sapVersion;
-	const sapDescription = req.body.sapDescription;
-	const sapFolder = req.body.sapFolder;
+    // Lite fields
+    const flowNameFolder = req.body.flowNameFolder;
+    const fuid = req.body.fuid;
+    const sbpVersion = req.body.sbpVersion;
+    const action1 = req.body.action1;
+    const action1Composite = req.body.action1Composite;
+    const servicio1Name = req.body.servicio1Name;
+    const servicio1Version = req.body.servicio1Version;
 
-	if(archselector != ''){
-		console.log(' [OK!! - Archetype selected]: Archetype selected - ' + archselector);
-	}else{
-		console.log('[KO - ANY archetype selected] - ' + archselector);
-	}
+    // SAP fields
+    const type = req.body.type;
+    const servicioName = req.body.servicioName;
+    const servicioVersion = req.body.servicioVersion;
+    const cacheable = req.body.cacheable;
+    const din = req.body.in;
+    const sapVersion = req.body.sapVersion;
+    const sapDescription = req.body.sapDescription;
+    const sapFolder = req.body.sapFolder;
 
-	switch (archselector)
-	{
-		case 'lite':
-		console.log("[POST to runCommand]: case LITE")
-		runCommand(res, {
-			archselector,	
-			groupId,
-			artifactId,
-			version,
-			packageName,
-			flowNameFolder,
-			fuid,
-			sbpVersion,
-			action1,
-			action1Composite,
-			servicio1Name,
-			servicio1Version
-		});
-		break;
+    // if (archselector != '') {
+    //     console.log('[OK!!]: Archetype selected - ' + archselector);
+    // } else {
+    //     console.log('[KO - ANY archetype selected] - ' + archselector);
+    // 	   Return message error to client
+    // }
 
-		case 'flow':
-		console.log("[POST to runCommand]: case FLOW")
-		runCommand(res, {
-			archselector,	
-			groupId,
-			artifactId,
-			version,
-			packageName,
-			flowNameFolder,
-			fuid,
-			sbpVersion,
-			action1,
-			action1Composite,
-			servicio1Name,
-			servicio1Version
-		});
-		break;
+    switch (archselector) {
+        case 'lite':
+            console.log("[POST to runCommand]: case LITE")
+            runCommand(res, {
+                archselector,
+                groupId,
+                artifactId,
+                version,
+                packageName,
+                flowNameFolder,
+                fuid,
+                sbpVersion,
+                action1,
+                action1Composite,
+                servicio1Name,
+                servicio1Version
+            });
+            break;
 
-		case 'sap':
-		console.log("[POST to runCommand]: case SAP")
-		runCommand(res, {
-			archselector,
-			groupId,
-			artifactId,
-			version,
-			packageName,
-			type,
-			servicioName,
-			servicioVersion,
-			cacheable,
-			din,
-			sapVersion,
-			sapDescription,
-			sapFolder
-		});
-		break;	
+        case 'flow':
+            console.log("[POST to runCommand]: case FLOW")
+            runCommand(res, {
+                archselector,
+                groupId,
+                artifactId,
+                version,
+                packageName,
+                flowNameFolder,
+                fuid,
+                sbpVersion,
+                action1,
+                action1Composite,
+                servicio1Name,
+                servicio1Version
+            });
+            break;
 
-		case 'enr':
-		console.log("[POST to runCommand]: case ENR")
-		runCommand(res, {
-			archselector,
-			groupId,
-			artifactId,
-			version,
-			packageName,
-			type,
-			servicioName,
-			servicioVersion,
-			cacheable,
-			din,
-			sapVersion,
-			sapDescription,
-			sapFolder
-		});
-		break;
-	}
+        case 'sap':
+            console.log("[POST to runCommand]: case SAP")
+            runCommand(res, {
+                archselector,
+                groupId,
+                artifactId,
+                version,
+                packageName,
+                type,
+                servicioName,
+                servicioVersion,
+                cacheable,
+                din,
+                sapVersion,
+                sapDescription,
+                sapFolder
+            });
+            break;
+
+        case 'enr':
+            console.log("[POST to runCommand]: case ENR")
+            runCommand(res, {
+                archselector,
+                groupId,
+                artifactId,
+                version,
+                packageName,
+                type,
+                servicioName,
+                servicioVersion,
+                cacheable,
+                din,
+                sapVersion,
+                sapDescription,
+                sapFolder
+            });
+            break;
+    }
 });
 
-function runCommand(res, params){
-	console.log(`[runCommand] archetype Selected: ${params.archselector}`);
+function runCommand(res, params) {
+    console.log(`[runCommand] Recived on runCommand: ${params.archselector}`);
 
-	switch (`${params.archselector}`)
-	{
-		case 'lite': // Run maven command LITE ==> [maven-archetype-quickstart]
-		console.log("[Processing runCommand]: case Lite")
-		nrc.run(`mvn.cmd archetype:generate -DgroupId=${params.groupId} -DartifactId=${params.artifactId} -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false`)
-		.then((exitCodes) => {
-			console.log(`exitcodes: ${exitCodes}`);
-			if (exitCodes == 0) {
-				compressAndSend(res, params);
-			}
-		}, (err) => {
-			console.log('error', err);
-		});
-		break;
+    switch (`${params.archselector}`) {
+        case 'lite': // Run maven command LITE ==> [maven-archetype-quickstart]
+            console.log("[Processing runCommand]: case Lite")
+            nrc.run(`mvn.cmd archetype:generate -DgroupId=${params.groupId} -DartifactId=${params.artifactId} -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false`)
+                .then((exitCodes) => {
+                    console.log(`exitcodes: ${exitCodes}`);
+                    if (exitCodes == 0) {
+                    	console.log(`[Command processed OK!]\n[Sending to compressAndSend]`);
+                        compressAndSend(res, params);
+                    }
+                }, (err) => {
+                    console.log('error', err);
+                });
+            break;
 
-		case 'flow': // Run maven command FLOW ==> [maven-archetype-j2ee-simple]
-		console.log("[Processing runCommand]: case FLOW")
-		nrc.run(`mvn.cmd archetype:generate -DgroupId=${params.groupId} -DartifactId=${params.artifactId} -DarchetypeArtifactId=maven-archetype-j2ee-simple -DinteractiveMode=false`)
-		.then((exitCodes) => {
-			console.log(`exitcodes: ${exitCodes}`);
-			if (exitCodes == 0) {
-				compressAndSend(res, params);
-			}
-		}, (err) => {
-			console.log('error', err);
-		});
-		break;
+        case 'flow': // Run maven command FLOW ==> [maven-archetype-j2ee-simple]
+            console.log("[Processing runCommand]: case FLOW")
+            nrc.run(`mvn.cmd archetype:generate -DgroupId=${params.groupId} -DartifactId=${params.artifactId} -DarchetypeArtifactId=maven-archetype-j2ee-simple -DinteractiveMode=false`)
+                .then((exitCodes) => {
+                    console.log(`exitcodes: ${exitCodes}`);
+                    if (exitCodes == 0) {
+                    	console.log(`[Command processed OK!]\n[Sending to compressAndSend]`);
+                        compressAndSend(res, params);
+                    }
+                }, (err) => {
+                    console.log('error', err);
+                });
+            break;
 
-		case 'sap': // Run maven command SAP ==> [maven-archetype-webapp]
-		console.log("[Processing runCommand]: case SAP")
-		nrc.run(`mvn.cmd archetype:generate -DgroupId=${params.groupId} -DartifactId=${params.artifactId} -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false`)
-		.then((exitCodes) => {
-			console.log(`exitcodes: ${exitCodes}`);
-			if (exitCodes == 0) {
-				compressAndSend(res, params);
-			}
-		}, (err) => {
-			console.log('error', err);
-		});
-		break;
+        case 'sap': // Run maven command SAP ==> [maven-archetype-webapp]
+            console.log("[Processing runCommand]: case SAP")
+            nrc.run(`mvn.cmd archetype:generate -DgroupId=${params.groupId} -DartifactId=${params.artifactId} -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false`)
+                .then((exitCodes) => {
+                    console.log(`exitcodes: ${exitCodes}`);
+                    if (exitCodes == 0) {
+                    	console.log(`[Command processed OK!]\n[Sending to compressAndSend]`);
+                        compressAndSend(res, params);
+                    }
+                }, (err) => {
+                    console.log('error', err);
+                });
+            break;
 
-		case 'enr': // Run maven command ENR ==> [archetype-site-simple]
-		console.log("[Processing runCommand]: case ENR")
-		nrc.run(`mvn.cmd archetype:generate -DgroupId=${params.groupId} -DartifactId=${params.artifactId} -DarchetypeArtifactId=archetype-site-simple -DinteractiveMode=false`)
-		.then((exitCodes) => {
-			console.log(`exitcodes: ${exitCodes}`);
-			if (exitCodes == 0) {
-				compressAndSend(res, params);
-			}
-		}, (err) => {
-			console.log('error', err);
-		});
-		break;
-	}
+        case 'enr': // Run maven command ENR ==> [archetype-site-simple]
+            console.log("[Processing runCommand]: case ENR")
+            nrc.run(`mvn.cmd archetype:generate -DgroupId=${params.groupId} -DartifactId=${params.artifactId} -DarchetypeArtifactId=archetype-site-simple -DinteractiveMode=false`)
+                .then((exitCodes) => {
+                    console.log(`exitcodes: ${exitCodes}`);
+                    if (exitCodes == 0) {
+                    	console.log(`[Command processed OK!]\n[Sending to compressAndSend]`);
+                        compressAndSend(res, params);
+                    }
+                }, (err) => {
+                    console.log('error', err);
+                });
+            break;
+    }
 }
 
 function compressAndSend(res, params) {
-	console.info('compressAndSend');
-	console.info(params);
+    console.info('[compressAndSend]:[IN]');
+    console.info(params);
 
-	zipFolder(path.join(__dirname, `/${params.artifactId}`), path.join(__dirname, `/${params.artifactId}.zip`), function(err){
-		if(err) {
-			console.log('oh no!', err);
-		} else {
-			sendToClient(res, params);
-			console.log('EXCELLENT');			
-		}
-	});
+    zipFolder(path.join(__dirname, `/${params.artifactId}`), path.join(__dirname, `/${params.artifactId}.zip`), function(err) {
+        if (err) {
+            console.log('Oh Nooo! Something went wrong during file compression...', err);
+        } else {
+        	console.info('[compressAndSend]: Success!! File compressed\n[calling to sendToClient]');
+            sendToClient(res, params);
+            console.log('EXCELLENT');
+        }
+    });
 }
 
 function sendToClient(res, params) {
-	console.info('sending...');
-	const filename = `${params.artifactId}.zip`;
-	// res.setHeader('content-type', 'application/zip');
-	// res.type('application/zip')
-	res.download(path.join(__dirname, './', filename));
-	deleteFilesAfterDownload(res, params);
+    console.info('sending...');
+    const filename = `${params.artifactId}.zip`;
+    // res.setHeader('content-type', 'application/zip');
+    // res.type('application/zip')
+    res.download(path.join(__dirname, './', filename));
+    deleteFilesAfterDownload(res, params);
 }
 
-function deleteFilesAfterDownload(res, params){
-	const folder = `${params.artifactId}`;
-	const zipFolder = `${params.artifactId}.zip`;
+function deleteFilesAfterDownload(res, params) {
+    const folder = `${params.artifactId}`;
+    const zipFolder = `${params.artifactId}.zip`;
 
-	rimraf(path.join(__dirname, './', folder), function () { console.log('Folder was deleted successfully!!'); });	
-	rimraf(path.join(__dirname, './', zipFolder), function () { console.log('Zip Folder was deleted successfully!!'); });
+    rimraf(path.join(__dirname, './', folder), function() {
+        console.log('Folder was deleted successfully!!');
+    });
+    rimraf(path.join(__dirname, './', zipFolder), function() {
+        console.log('Zip Folder was deleted successfully!!');
+    });
 }
 
-app.listen(3000, function () {
-	console.log('bnk-Builder app listening on port 3000!');
+app.listen(3000, function() {
+    console.log('bnk-Builder app listening on port 3000!');
 });
-
-
--- public/js/cmd.js
+++ public/js/cmd.js
@@ -1,54 +1,54 @@
-
-
 function generatePackageName() {
-	var groupId = document.forms['form']['groupId'].value;
-	var artifactId = document.forms['form']['artifactId'].value;
+    var groupId = document.forms['form']['groupId'].value;
+    var artifactId = document.forms['form']['artifactId'].value;
 
-	if (groupId != "" || artifactId != "") {
-		document.forms['form']['packageName'].value = groupId + "."	+ artifactId;
-	} else if (groupId === "" || artifactId === "") {
-		document.forms['form']['packageName'].value = "";
-	}
+    if (groupId != "" || artifactId != "") {
+        document.forms['form']['packageName'].value = groupId + "." + artifactId;
+    } else if (groupId === "" || artifactId === "") {
+        document.forms['form']['packageName'].value = "";
+    }
 }
 
 
 
 $(document).ready(function() {
 
-	/* this activates the "which columns to display" selectbox */
-	
-	// ARCHEYPE Selector
-	$('#archetypeSelector').on('change',function() {
-		var e = document.getElementById("archetypeSelector");
-		var selectedOption = e.options[e.selectedIndex].value;
-		console.log('[ready]:[archetypeSelector]: showing: ' + selectedOption);
+    /* this activates the "which columns to display" selectbox */
 
-		// set fields according archetype 
-		showInputsByArchetype(selectedOption);
-	});
-	// Init archetype selector when document.ready
-	$('#archetypeSelector').material_select();
+    // ARCHEYPE Selector
+    $('#archetypeSelector').on('change', function() {
+        var e = document.getElementById("archetypeSelector");
+        var selectedOption = e.options[e.selectedIndex].value;
+        console.log('[ready]:[archetypeSelector]: showing: ' + selectedOption);
 
-
-	// SAP TYPE Selector
-	$('#sapSelector').on('change',function() {
-		var e = document.getElementById("sapSelector");
-		var typeSelected = e.options[e.selectedIndex].value;
-		console.log('[ready]:[sapSelector]: showing sap type: ' + typeSelected);
-
-			// set fields according archetype 
-			showInputsBySapType(typeSelected);
-		});
-	// Init sapType selector when document.ready
-	$('#sapSelector').material_select();
+        // set fields according archetype 
+        showInputsByArchetype(selectedOption);
+    });
+    // Init archetype selector when document.ready
+    $('#archetypeSelector').material_select();
 
 
+    // SAP TYPE Selector
+    $('#sapSelector').on('change', function() {
+        var e = document.getElementById("sapSelector");
+        var typeSelected = e.options[e.selectedIndex].value;
+        console.log('[ready]:[sapSelector]: showing sap type: ' + typeSelected);
 
-	// Inicializa tootip cuando está ready
-	$('.tooltipped').tooltip({delay: 50});
+        // set fields according archetype 
+        showInputsBySapType(typeSelected);
+    });
+    // Init sapType selector when document.ready
+    $('#sapSelector').material_select();
 
-	// Inicializa actualiza inputs cuando está ready
-	Materialize.updateTextFields();
+
+
+    // Init tootip
+    $('.tooltipped').tooltip({
+        delay: 50
+    });
+
+    // Init updateTextField
+    Materialize.updateTextFields();
 
 });
 
@@ -56,99 +56,99 @@ $(document).ready(function() {
 
 
 // Set fields according archetype
-function showInputsByArchetype(selectedOption){
-	console.log('recibido en showInputsByArchetype: ' + selectedOption);
+function showInputsByArchetype(selectedOption) {
+    console.log('recibido en showInputsByArchetype: ' + selectedOption);
 
-	switch (selectedOption)
-	{
+    switch (selectedOption) {
 
-		case 'lite': 
-			console.log("[showInputsByArchetype]: case LITE")
-			// Ocultar campos del SAP al no requerirse en el Lite
-			document.getElementById('type').style.display = 'none';
-			document.getElementById('servicioName').style.display = 'none';
-			document.getElementById('servicioVersion').style.display = 'none';
-			document.getElementById('cacheable').style.display = 'none';
-			document.getElementById('in').style.display = 'none';
-			document.getElementById('sapVersion').style.display = 'none';
-			document.getElementById('sapDescription').style.display = 'none';
-			document.getElementById('sapFolder').style.display = 'none';	
+        case 'lite':
+            console.log("[showInputsByArchetype]: case LITE")
+            // Hide SAP fields when Lite is selected
+            document.getElementById('type').style.display = 'none';
+            document.getElementById('servicioName').style.display = 'none';
+            document.getElementById('servicioVersion').style.display = 'none';
+            document.getElementById('cacheable').style.display = 'none';
+            document.getElementById('in').style.display = 'none';
+            document.getElementById('sapVersion').style.display = 'none';
+            document.getElementById('sapDescription').style.display = 'none';
+            document.getElementById('sapFolder').style.display = 'none';
 
-			// Mostrar el conjunto de campos del LITE
-			document.getElementById('flowNameFolder').style.display = 'block';
-			document.getElementById('fuid').style.display = 'block';
-			document.getElementById('sbpVersion').style.display = 'block';
-			document.getElementById('action1').style.display = 'block';
-			document.getElementById('action1Composite').style.display = 'block';
-			document.getElementById('servicio1Name').style.display = 'block';
-			document.getElementById('servicio1Version').style.display = 'block';
-			break;
-
-
-		case 'flow': 
-			console.log("[showInputsByArchetype]: case FLOW")
-			break;
+            // Show set Lite fields
+            document.getElementById('flowNameFolder').style.display = 'block';
+            document.getElementById('fuid').style.display = 'block';
+            document.getElementById('sbpVersion').style.display = 'block';
+            document.getElementById('action1').style.display = 'block';
+            document.getElementById('action1Composite').style.display = 'block';
+            document.getElementById('servicio1Name').style.display = 'block';
+            document.getElementById('servicio1Version').style.display = 'block';
+            break;
 
 
-
-		case 'sap': 
-			console.log("[showInputsByArchetype]: case SAP")
-			// Ocultamos campos del Lite al no requerirse en el SAP
-			document.getElementById('flowNameFolder').style.display = 'none';
-			document.getElementById('fuid').style.display = 'none';
-			document.getElementById('sbpVersion').style.display = 'none';
-			document.getElementById('action1').style.display = 'none';
-			document.getElementById('action1Composite').style.display = 'none';
-			document.getElementById('servicio1Name').style.display = 'none';
-			document.getElementById('servicio1Version').style.display = 'none';
-
-			// Mostrar el conjunto de campos del SAP
-			document.getElementById('type').style.display = 'block';
-			document.getElementById('servicioName').style.display = 'block';
-			document.getElementById('servicioVersion').style.display = 'block';
-			document.getElementById('cacheable').style.display = 'block';
-			document.getElementById('in').style.display = 'block';
-			document.getElementById('sapVersion').style.display = 'block';
-			document.getElementById('sapDescription').style.display = 'block';
-			document.getElementById('sapFolder').style.display = 'block';
-			break;
-
-
-		case 'enr': 
-			console.log("[showInputsByArchetype]: case ENR")
-			break;
+        case 'flow':
+            console.log("[showInputsByArchetype]: case FLOW")
+            break;
 
 
 
-		default:  console.log("[showInputsByArchetype]: default")
+        case 'sap':
+            console.log("[showInputsByArchetype]: case SAP")
+            // Hide Lite fields when SAP is selected
+            document.getElementById('flowNameFolder').style.display = 'none';
+            document.getElementById('fuid').style.display = 'none';
+            document.getElementById('sbpVersion').style.display = 'none';
+            document.getElementById('action1').style.display = 'none';
+            document.getElementById('action1Composite').style.display = 'none';
+            document.getElementById('servicio1Name').style.display = 'none';
+            document.getElementById('servicio1Version').style.display = 'none';
+
+            // Show set SAP fields
+            document.getElementById('type').style.display = 'block';
+            document.getElementById('servicioName').style.display = 'block';
+            document.getElementById('servicioVersion').style.display = 'block';
+            document.getElementById('cacheable').style.display = 'block';
+            document.getElementById('in').style.display = 'block';
+            document.getElementById('sapVersion').style.display = 'block';
+            document.getElementById('sapDescription').style.display = 'block';
+            document.getElementById('sapFolder').style.display = 'block';
+            break;
+
+
+        case 'enr':
+            console.log("[showInputsByArchetype]: case ENR")
+            break;
+
+
+
+        default:
+            console.log("[showInputsByArchetype]: default")
             //document.write("Exiting switch block");
 
-        }
     }
+}
 
 
-    //showInputsBySapType
-    function showInputsBySapType(typeSelected){
+//showInputsBySapType
+function showInputsBySapType(typeSelected) {
 
-    	switch (typeSelected)	{
+    switch (typeSelected) {
 
-    		case 'sit': 
-    			console.log("[showInputsBySapType]: case SAP-SIT")
-				// Mostramos si campos ocultados por 'case tabit'
-				document.getElementById('in').style.display = 'block';
-				break;
+        case 'sit':
+            console.log("[showInputsBySapType]: case SAP-SIT")
+            // Show SAP-SIT field, hidden by the TABIT case
+            document.getElementById('in').style.display = 'block';
+            break;
 
-			case 'tabit': 
-				console.log("[showInputsBySapType]: case SAP-TABIT")
-				// Ocultamos campos del Lite al no requerirse en el SAP
-				document.getElementById('in').style.display = 'none';
-				break;
+        case 'tabit':
+            console.log("[showInputsBySapType]: case SAP-TABIT")
+            // Hide SAP-SIT-SNG fields when TABIT is selected
+            document.getElementById('in').style.display = 'none';
+            break;
 
-			case 'sng': 
-				console.log("[showInputsBySapType]: case SAP-SNG")
-				// Mostramos si campos ocultados por 'case tabit'
-				document.getElementById('in').style.display = 'block';
-				break;
+        case 'sng':
+            console.log("[showInputsBySapType]: case SAP-SNG")
+            // Show SAP-SNG field, hidden by the TABIT case
+            document.getElementById('in').style.display = 'block';
+            break;
 
-			}
-		}
+    }
+}
